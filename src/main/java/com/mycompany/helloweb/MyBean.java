bce63d131af7d25838109d97e555e38240ce7874
gailo22 <gailo22@gmail.com>
2013-06-17 15:20:30 +0700

    fixed for deploying to websphere 8.5

--- src/main/java/com/mycompany/helloweb/MyBean.java
+++ src/main/java/com/mycompany/helloweb/MyBean.java
@@ -4,8 +4,11 @@
  */
 package com.mycompany.helloweb;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
 import org.primefaces.context.RequestContext;
 
 /**
@@ -14,24 +17,26 @@ import org.primefaces.context.RequestContext;
  */
 @ManagedBean
 @ViewScoped
-public class MyBean {
-    
-    private String name;
-    
-    public void doSomething() {
-        final RequestContext requestContext = RequestContext.getCurrentInstance();
-        requestContext.addCallbackParam("callBackParam", "done.");
+public class MyBean implements Serializable {
 
-        System.out.println("doSomething..");
-        System.out.println("name : " + this.name);
-    }
+	private static final long serialVersionUID = 4370199033249412014L;
 
-    public String getName() {
-        return name;
-    }
+	private String name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    
+	public void doSomething() {
+		final RequestContext requestContext = RequestContext.getCurrentInstance();
+		requestContext.addCallbackParam("callBackParam", "done.");
+
+		System.out.println("doSomething..");
+		System.out.println("name : " + this.name);
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
 }
